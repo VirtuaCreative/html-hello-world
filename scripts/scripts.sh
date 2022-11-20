eae15a4e1216009cd505991f5bdf813cb0e01f0a
Marcia Ramos <virtua.creative@gmail.com>
2022-11-20 14:18:44 +0000

    Update script

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-docker build -t html-hello-world:v1 .
+docker build -t html-hello-world:v2 .
 
-docker run -d -p 80:80 html-hello-world:v1
+docker run -d -p 80:80 html-hello-world:v2
 
-docker tag html-hello-world:v1 ramosmd/html-hello-world
+docker tag html-hello-world:v2 ramosmd/html-hello-world:v2
 
-docker push ramosmd/html-hello-world
+docker push ramosmd/html-hello-world:v2
