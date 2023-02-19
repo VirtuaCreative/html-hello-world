a1fe56edf0f957d6b3da4e10745ff6fb1440069d
Marcia Ramos <virtua.creative@gmail.com>
2023-02-19 13:31:31 +0000

    Minor fixes

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -3,14 +3,13 @@
 # Versioned
 
 docker build -t html-hello-world:v2.1 .
-
 docker run -d -p 80:80 html-hello-world:v2.1
 
 docker tag html-hello-world:v2.1 ramosmd/html-hello-world:v2.1
-
 docker push ramosmd/html-hello-world:v2.1
 
 # Latest
 
+docker build -t html-hello-world:latest .
 docker tag html-hello-world:latest ramosmd/html-hello-world:latest
 docker push ramosmd/html-hello-world:latest
